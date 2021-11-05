51655f9108065a8d39b10d060d82fe4802df69d9
oreyphews <oreyphews@gmail.com>
2021-11-05 21:26:23 +0800

    another test

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AcadyrController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\DepartmentSingleController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AnnounceMentsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,18 +19,26 @@ use App\Http\Controllers\DepartmentSingleController;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+// Route::get('/', function () {
+//     return view('login');
+// });
+Route::get('/', [AuthController::class, 'index'])->name('login');
+Route::post('/', [AuthController::class, 'signIn'])->name('signin');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('admin/dashboard', function () {
     return view('admin/dashboard');
-});
+})->middleware('auth');
+
+Route::get('admin/dashboard', [AnnounceMentsController::class, 'displayAnnouncements']);
+Route::post('admin/dashboard', [AnnounceMentsController::class, 'addAnnouncement'])->name('addAnnouncement');
+
+
+
 
 Route::get('admin/accounts', function () {
     return view('admin/accounts');
 });
-
 Route::post('admin/accounts', [UserController::class, 'addNewUser']);
 Route::get('admin/accounts', [UserController::class, 'showDepartments']);
 
